d34b08019d19dd1d774a3cd9cc168d33c1158bbf
ShenZhi <110948752+smiledecade@users.noreply.github.com>
2024-05-25 02:41:46 +0800

    Update zhuaqu3.py

--- zhuaqu3.py
+++ zhuaqu3.py
@@ -85,8 +85,10 @@ def send_email_with_attachment(subject, body, attachment_path, sender_email, sen
 
 def main():
     # 获取最大文章ID
-    end_id = fetch_max_article_id()-5
-    start_id = end_id - 10
+    end_id = 607341
+    #fetch_max_article_id() - 5
+    start_id = 607340
+    #end_id - 10
 
     # 开始抓取文章
     base_url = 'https://www.qm120.com/zt/baike/{}.html'
